46c3252a8b735baeb038749fed72e76ccdc63563
PrudviSid91 <PrudhviSiddharth@outlook.com>
2020-07-21 13:45:29 +0530

    Added Socket IO module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const publicDirectoryPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New Connection Setup')
+})
+
+server.listen(port, () => {
     console.log('Server is up and running on port', port)
 })
