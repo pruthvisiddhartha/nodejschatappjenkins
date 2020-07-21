dae53e344218ea25f369e8e3be1bb70782e2f217
PrudviSid91 <PrudhviSiddharth@outlook.com>
2020-07-21 11:11:50 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname,'../public')
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+    console.log('Server is up and running on port', port)
+})
